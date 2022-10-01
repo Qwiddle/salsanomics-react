702f05e3a02d76d2377c30479a3e84571bec4527
Andrew Rubenstein <andjruben@gmail.com>
2022-10-01 00:42:32 -0400

    create TokenCopy component
    token & logo for FarmCard

---
+++ src/components/TokenCopy/TokenCopy.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import P from '../P';
+
+export interface IAppProps {
+  ticker: string;
+  Logo: React.ComponentType;
+}
+
+const TokenDetail = styled.div`
+  width: 80px;
+  display: flex;
+  align-items: center;
+`;
+
+export default function TokenCopy(props: IAppProps): JSX.Element {
+  const { ticker, Logo } = props;
+
+  return (
+    <TokenDetail>
+      <Logo />
+      <P>{ticker}</P>
+    </TokenDetail>
+  );
+}
