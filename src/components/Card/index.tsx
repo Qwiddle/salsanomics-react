cee4fe3eb17c6af980e9f7eaad66bc33c74c89e0
Andrew <andjruben@gmail.com>
2022-10-01 11:58:30 -0400

    small Card refactor
    separate all card components into it's own file

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+  min-width: 340px;
+  min-height: 400px;
+  display: flex;
+  padding: 10px;
+  gap: 10px;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background: #fcfcfc;
+  box-shadow: 0px 80px 105px rgba(0, 0, 0, 0.07),
+    0px 10px 13px rgba(0, 0, 0, 0.03);
+  border-radius: 24px 24px 24px 24px;
+`;
+
+export const CardHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  width: 250px;
+  min-height: 40px;
+  background: #f0f0f0;
+  border-radius: 8px;
+`;
+
+export const CardHeaderText = styled.p`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 24px;
+  text-transform: capitalize;
+`;
+
+export const CardBox = styled.div`
+  min-width: 80px;
+  border-radius: 8px;
+  background: #f0f0f0;
+  padding: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const CardBody = styled.div`
+  flex: 1;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
--- src/pages/Analytics/Analytics.tsx
+++ src/pages/Analytics/Analytics.tsx
@@ -1,9 +1,14 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import styled from 'styled-components';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { ReactComponent as SalsaToken } from './assets/spicyswap_logo.svg';
 import TokenCopy from '../../components/TokenCopy';
 import P from '../../components/P';
+import {
+  Card,
+  CardBody,
+  CardBox,
+  CardHeader,
+  CardHeaderText,
+} from '../../components/Card';
+import { ReactComponent as SalsaToken } from './assets/spicyswap_logo.svg';
 
 const PageWrapper = styled.section`
   padding: 1em;
@@ -15,65 +20,12 @@ const PageWrapper = styled.section`
   flex-wrap: wrap;
 `;
 
-const FarmCard = styled.div`
-  min-width: 340px;
-  min-height: 400px;
-  display: flex;
-  padding: 10px;
-  gap: 10px;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  background: #fcfcfc;
-  box-shadow: 0px 80px 105px rgba(0, 0, 0, 0.07),
-    0px 10px 13px rgba(0, 0, 0, 0.03);
-  border-radius: 24px 24px 24px 24px;
-`;
-
-const CardHeader = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-around;
-  width: 250px;
-  min-height: 40px;
-  background: #f0f0f0;
-  border-radius: 8px;
-`;
-
-const HeaderText = styled.p`
-  font-family: 'Inter';
-  font-style: normal;
-  font-weight: 600;
-  font-size: 20px;
-  line-height: 24px;
-  text-transform: capitalize;
-`;
-
-const CardBox = styled.div`
-  min-width: 80px;
-  border-radius: 8px;
-  background: #f0f0f0;
-  padding: 8px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const CardBody = styled.div`
-  flex: 1;
-  width: 300px;
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-`;
-
 export default function Analytics(): JSX.Element {
   return (
     <PageWrapper>
-      <FarmCard>
+      <Card>
         <CardHeader>
-          <HeaderText>SpicySwap 🌶️</HeaderText>
+          <CardHeaderText>SpicySwap 🌶️</CardHeaderText>
         </CardHeader>
         <CardBody>
           <CardBox>
@@ -89,7 +41,7 @@ export default function Analytics(): JSX.Element {
             <P>712 ꜩ</P>
           </CardBox>
         </CardBody>
-      </FarmCard>
+      </Card>
     </PageWrapper>
   );
 }
