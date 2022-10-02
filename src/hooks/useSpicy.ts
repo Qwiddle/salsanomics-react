a3577c696764f2537c6a292b8caee8127645ec07
Andrew <andjruben@gmail.com>
2022-10-02 01:19:43 -0400

    update useSpicy hook

--- src/hooks/useSpicy.ts
+++ src/hooks/useSpicy.ts
@@ -1,20 +1,22 @@
 import { useEffect, useState } from 'react';
-import { ISpicyPool, ISpicyToken } from '../const/ecosystem';
-import { fetchSpicyPools, fetchSpicyTokens } from './api/spicy';
+import { ISpicyMetric, ISpicyPool, ISpicyToken } from '../const/ecosystem';
+import { fetchSpicyPools, fetchSpicyTokens, fetchDailyMetrics } from './api/spicy';
 
 const useSpicy = () => {
   const [loading, setLoading] = useState<Boolean>(true);
   const [tokens, setTokens] = useState<ISpicyToken[]>();
   const [pools, setPools] = useState<ISpicyPool[]>();
-
-  setLoading(false);
+  const [metrics, setMetrics] = useState<ISpicyMetric[]>();
 
   const fetchSpicy = async () => {
     const sTokens = await fetchSpicyTokens();
     const sPools = await fetchSpicyPools();
+    const sMetrics = await fetchDailyMetrics();
 
     setTokens(sTokens);
     setPools(sPools);
+    setMetrics(sMetrics);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -25,6 +27,7 @@ const useSpicy = () => {
     loading,
     tokens,
     pools,
+    metrics,
   };
 };
 
