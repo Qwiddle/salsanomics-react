f696c0e291f92c68a672c986aa18ce4a2b32b319
Andrew <andjruben@gmail.com>
2022-10-02 20:46:00 -0400

    update Card styling

--- src/components/Card.ts
+++ src/components/Card.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Card = styled.div`
   min-width: 340px;
-  min-height: 400px;
+  height: 400px;
   display: flex;
   padding: 10px;
   gap: 10px;
@@ -10,8 +10,7 @@ export const Card = styled.div`
   justify-content: space-between;
   align-items: center;
   background: #fcfcfc;
-  box-shadow: 0px 80px 105px rgba(0, 0, 0, 0.07),
-    0px 10px 13px rgba(0, 0, 0, 0.03);
+  box-shadow: 0px 80px 105px rgba(0, 0, 0, 0.07), 0px 10px 13px rgba(0, 0, 0, 0.03);
   border-radius: 24px 24px 24px 24px;
 `;
 
@@ -22,8 +21,10 @@ export const CardHeader = styled.div`
   justify-content: space-around;
   width: 250px;
   min-height: 40px;
-  background: #f0f0f0;
-  border-radius: 8px;
+  border: 1px solid rgba(27, 31, 35, 0.15);
+  border-radius: 6px;
+  box-shadow: rgba(27, 31, 35, 0.04) 0 1px 0, rgba(255, 255, 255, 0.25) 0 1px 0 inset;
+  box-sizing: border-box;
 `;
 
 export const CardHeaderText = styled.p`
@@ -38,8 +39,10 @@ export const CardHeaderText = styled.p`
 export const CardBox = styled.div`
   min-width: 80px;
   min-height: 50px;
-  border-radius: 8px;
-  background: #f0f0f0;
+  border: 1px solid rgba(27, 31, 35, 0.15);
+  border-radius: 6px;
+  box-shadow: rgba(27, 31, 35, 0.04) 0 1px 0, rgba(255, 255, 255, 0.25) 0 1px 0 inset;
+  box-sizing: border-box;
   padding: 8px;
   display: flex;
   align-items: center;
@@ -49,7 +52,7 @@ export const CardBox = styled.div`
 
 export const CardBody = styled.div`
   flex: 1;
-  width: 300px;
+  width: 85%;
   display: flex;
   flex-direction: column;
   gap: 8px;
