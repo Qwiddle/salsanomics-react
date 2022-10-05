a52df32cda74893bb3b6ed391f93eb90d15eeacd
Andrew <andjruben@gmail.com>
2022-10-05 00:16:30 -0400

    improve & refactor getEventDetails
    now supports reading multiple different contracts & matching all buyins according to the date the contest was active.
    
    need to go back and provide proper types here.

--- src/hooks/api/tzkt.ts
+++ src/hooks/api/tzkt.ts
@@ -1,58 +1,62 @@
-import { ICasinoEvent } from '../../const/ecosystem';
+import { ICasinoEvent, CasinoEvent } from '../../const/ecosystem';
 
 const TZKT_API = `https://api.tzkt.io/v1`;
 
-// todo: create type for event buy in
-export const getEventBuyIns = async (): Promise<any> => {
-  const res = await fetch(
-    `${TZKT_API}/accounts/KT1DdxRFoVEjE2FtsuEL1p2iippu6xCw8XhS/operations?entrypoint=buyIn&limit=300`
-  );
+const casinoContracts = ['KT1Q3Z9VwmSG6wFNLebD9yRm7PENXHJxHn3n', 'KT1DdxRFoVEjE2FtsuEL1p2iippu6xCw8XhS'];
+const casinoMappings: Map<string, CasinoEvent> = new Map([
+  ['KT1DdxRFoVEjE2FtsuEL1p2iippu6xCw8XhS', 'standard'],
+  ['KT1Q3Z9VwmSG6wFNLebD9yRm7PENXHJxHn3n', 'high'],
+]);
 
+// todo: create type for event buy in
+export const getEventBuyIns = async (contract: string): Promise<any> => {
+  const res = await fetch(`${TZKT_API}/accounts/${contract}/operations?entrypoint=buyIn&limit=300`);
   const json = await res.json();
   return json;
 };
 
 export const getEventDetails = async (): Promise<ICasinoEvent[]> => {
-  const res = await fetch(`${TZKT_API}/contracts/KT1DdxRFoVEjE2FtsuEL1p2iippu6xCw8XhS/storage/history`);
-  const json = await res.json();
+  const casinoEvents = await Promise.all<any>(
+    casinoContracts.map(async (contract) => {
+      const buyIns = await getEventBuyIns(contract);
 
-  const buyIns = await getEventBuyIns();
+      const res = await fetch(`${TZKT_API}/contracts/${contract}/storage/history`);
+      const json = await res.json();
 
-  const events = json
-    .filter((e: any) => {
-      if (e.operation.parameter) {
-        const entryPoint = e.operation.parameter.entrypoint;
-        return entryPoint === 'startContest';
-      }
+      return json
+        .filter((f: any) => {
+          if (f.operation.parameter) {
+            const entryPoint = f.operation.parameter.entrypoint;
+            return entryPoint === 'startContest';
+          }
 
-      return false;
+          return false;
+        })
+        .map((e: any) => {
+          const now = buyIns.filter((b: any) => {
+            const oTimestamp = new Date(b.timestamp);
+            return oTimestamp > new Date(e.timestamp) && oTimestamp < new Date(e.value.ending);
+          });
+
+          const buyIn = Number(e.value.buy_in) / 10 ** 6;
+          const participants = now.length;
+          const pot = buyIn * participants;
+          const type = casinoMappings.get(contract);
+
+          return {
+            participants,
+            pot,
+            buyIn,
+            type,
+            buyFee: Number(e.value.buy_in_fee),
+            start: new Date(e.timestamp),
+            end: new Date(e.value.ending),
+          };
+        });
     })
-    .map((e: any) => {
-      const now = buyIns.filter((b: any) => {
-        const oTimestamp = new Date(b.timestamp);
+  );
 
-        if (oTimestamp > new Date(e.timestamp) && oTimestamp < new Date(e.value.ending)) {
-          return true;
-        }
-
-        return false;
-      });
-
-      const buyIn = Number(e.value.buy_in) / 10 ** 6;
-      const participants = now.length;
-      const pot = buyIn * participants;
-
-      return {
-        participants,
-        pot,
-        buyIn,
-        buyFee: Number(e.value.buy_in_fee),
-        start: new Date(e.timestamp),
-        end: new Date(e.value.ending),
-      };
-    });
-
-  return events;
+  return casinoEvents.flat();
 };
 
 export const fetchBurns = async (): Promise<number> => {
