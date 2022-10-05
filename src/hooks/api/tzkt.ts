3210a7f9f5d2ea98e0f0a72043c026a72d025648
Andrew <andjruben@gmail.com>
2022-10-05 01:25:56 -0400

    small code cleanup & add burns

--- src/hooks/api/tzkt.ts
+++ src/hooks/api/tzkt.ts
@@ -1,15 +1,16 @@
-import { ICasinoEvent, CasinoEvent } from '../../const/ecosystem';
+import { ICasinoEvent, CasinoEvent, SDAO, BURNER } from '../../const/ecosystem';
 
 const TZKT_API = `https://api.tzkt.io/v1`;
 
 const casinoContracts = ['KT1Q3Z9VwmSG6wFNLebD9yRm7PENXHJxHn3n', 'KT1DdxRFoVEjE2FtsuEL1p2iippu6xCw8XhS'];
+
 const casinoMappings: Map<string, CasinoEvent> = new Map([
   ['KT1DdxRFoVEjE2FtsuEL1p2iippu6xCw8XhS', 'standard'],
   ['KT1Q3Z9VwmSG6wFNLebD9yRm7PENXHJxHn3n', 'high'],
 ]);
 
 export const getBurns = async (): Promise<number> => {
-  const req = `${TZKT_API}/tokens/balances?account=KT1CZMurPAjSfZqcn6LBUNUhG4byE6AJgDT6&token.contract=KT19ovJhcsUn4YU8Q5L3BGovKSixfbWcecEA`;
+  const req = `${TZKT_API}/tokens/balances?account=${BURNER}&token.contract=${SDAO}`;
   const res = await fetch(req);
 
   if (res.ok) {
@@ -47,22 +48,27 @@ export const getEventDetails = async (): Promise<ICasinoEvent[]> => {
           return false;
         })
         .map((e: any) => {
-          const now = buyIns.filter((b: any) => {
+          const active = buyIns.filter((b: any) => {
             const oTimestamp = new Date(b.timestamp);
             return oTimestamp > new Date(e.timestamp) && oTimestamp < new Date(e.value.ending);
           });
 
-          const buyIn = Number(e.value.buy_in) / 10 ** 6;
-          const participants = now.length;
-          const pot = buyIn * participants;
+          const participants = active.length;
           const type = casinoMappings.get(contract);
+
+          const buyIn = Number(e.value.buy_in) / 10 ** 6;
+          const buyFee = Number(e.value.buy_in_fee);
+
+          const pot = buyIn * participants;
+          const burn = buyFee * participants;
 
           return {
             participants,
             pot,
             buyIn,
             type,
-            buyFee: Number(e.value.buy_in_fee),
+            buyFee,
+            burn,
             start: new Date(e.timestamp),
             end: new Date(e.value.ending),
           };
