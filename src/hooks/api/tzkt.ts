13ccf5805567188ac96259e7824eefe7b85d749f
Andrew Rubenstein <andjruben@gmail.com>
2022-10-13 20:26:19 -0400

    refactor getEventDetails
    split up the logic into separate functions

--- src/hooks/api/tzkt.ts
+++ src/hooks/api/tzkt.ts
@@ -9,6 +9,66 @@ const casinoMappings: Map<string, CasinoEvent> = new Map([
   ['KT1Q3Z9VwmSG6wFNLebD9yRm7PENXHJxHn3n', 'high'],
 ]);
 
+// todo: create type for event buy in
+export const getEventBuyIns = async (contract: string): Promise<any> => {
+  const req = `${TZKT_API}/accounts/${contract}/operations?entrypoint=buyIn&limit=300`;
+  const res = await fetch(req);
+
+  if (!res.ok) throw new Error(`Failed to fetch Event BuyIns, ${res.status}`);
+
+  const json = await res.json();
+  return json;
+};
+
+const transformEvents = (events: any, contract: string, buyIns: []): any => {
+  return events
+    .filter((f: any) => {
+      if (f.operation.parameter) {
+        const entryPoint = f.operation.parameter.entrypoint;
+        return entryPoint === 'startContest';
+      }
+
+      return false;
+    })
+    .map((e: any) => {
+      const active = buyIns.filter((b: any) => {
+        const oTimestamp = new Date(b.timestamp);
+        return oTimestamp > new Date(e.timestamp) && oTimestamp < new Date(e.value.ending);
+      });
+
+      const participants = active.length;
+      const type = casinoMappings.get(contract);
+
+      const buyIn = Number(e.value.buy_in) / 10 ** 6;
+      const buyFee = Number(e.value.buy_in_fee);
+
+      const pot = buyIn * participants;
+      const burn = buyFee * participants;
+
+      return {
+        participants,
+        pot,
+        buyIn,
+        type,
+        buyFee,
+        burn,
+        buyIns: active,
+        start: new Date(e.timestamp),
+        end: new Date(e.value.ending),
+      };
+    });
+};
+
+const getEventsByContract = async (contract: string): Promise<any> => {
+  const res = await fetch(`${TZKT_API}/contracts/${contract}/storage/history`);
+  const json = await res.json();
+
+  const buyIns = await getEventBuyIns(contract);
+  const transformed = transformEvents(json, contract, buyIns);
+
+  return transformed;
+};
+
 export const getBurns = async (): Promise<number> => {
   const req = `${TZKT_API}/tokens/balances?account=${BURNER}&token.contract=${SDAO}`;
   const res = await fetch(req);
@@ -21,63 +81,8 @@ export const getBurns = async (): Promise<number> => {
   return burnAmount;
 };
 
-// todo: create type for event buy in
-export const getEventBuyIns = async (contract: string): Promise<any> => {
-  const req = `${TZKT_API}/accounts/${contract}/operations?entrypoint=buyIn&limit=300`;
-  const res = await fetch(req);
-
-  if (!res.ok) throw new Error(`Failed to fetch Event BuyIns, ${res.status}`);
-
-  const json = await res.json();
-  return json;
-};
-
 export const getEventDetails = async (): Promise<ICasinoEvent[]> => {
-  const casinoEvents = await Promise.all<any>(
-    casinoContracts.map(async (contract) => {
-      const buyIns = await getEventBuyIns(contract);
-
-      const res = await fetch(`${TZKT_API}/contracts/${contract}/storage/history`);
-      const json = await res.json();
-
-      return json
-        .filter((f: any) => {
-          if (f.operation.parameter) {
-            const entryPoint = f.operation.parameter.entrypoint;
-            return entryPoint === 'startContest';
-          }
-
-          return false;
-        })
-        .map((e: any) => {
-          const active = buyIns.filter((b: any) => {
-            const oTimestamp = new Date(b.timestamp);
-            return oTimestamp > new Date(e.timestamp) && oTimestamp < new Date(e.value.ending);
-          });
-
-          const participants = active.length;
-          const type = casinoMappings.get(contract);
-
-          const buyIn = Number(e.value.buy_in) / 10 ** 6;
-          const buyFee = Number(e.value.buy_in_fee);
-
-          const pot = buyIn * participants;
-          const burn = buyFee * participants;
-
-          return {
-            participants,
-            pot,
-            buyIn,
-            type,
-            buyFee,
-            burn,
-            buyIns: active,
-            start: new Date(e.timestamp),
-            end: new Date(e.value.ending),
-          };
-        });
-    })
-  );
+  const casinoEvents = await Promise.all<any>(casinoContracts.map(getEventsByContract));
 
   return casinoEvents.flat();
 };
