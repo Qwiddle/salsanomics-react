990e05115b450c6362901b0f8f878f4ac5fbcb7a
Andrew <andjruben@gmail.com>
2022-10-27 22:51:39 -0400

    create supporting map & conversion functions

--- src/hooks/api/matter.ts
+++ src/hooks/api/matter.ts
@@ -1,6 +1,137 @@
+import BigNumber from 'bignumber.js';
 import { getSupply, getTokenName, TZKT_API } from './tzkt';
-import { MTTR } from '../../const/ecosystem';
+import { ISpicyPool, ISpicyToken, MTTR } from '../../const/ecosystem';
 import { fetchTokenPrice } from './spicy';
+
+const lpToTez = (staked: BigNumber, farm: any) => {
+  if (!farm.single) {
+    const { reserve, supply, decimals } = farm.pool;
+    const tezPerLp = BigNumber(reserve).dividedBy(BigNumber(supply).shiftedBy(-decimals));
+    const lpValue = tezPerLp.multipliedBy(BigNumber(staked).shiftedBy(-decimals));
+
+    return lpValue.toFixed(2);
+  }
+  const { derivedXtz, decimals } = farm.token;
+  const tokenValue = BigNumber(derivedXtz).multipliedBy(BigNumber(staked).shiftedBy(-decimals));
+
+  return tokenValue.toFixed(2);
+};
+
+export const matchFarms = (
+  spicyPools: ISpicyPool[],
+  spicyTokens: ISpicyToken[],
+  farms: any,
+  configs: any,
+  balances: any
+) => {
+  const today = new Date();
+  const active = new Date(configs[0].active_time);
+
+  const activeConfig = today.getTime() >= active.getTime() ? configs[0].farm_configs : configs[1].farm_configs;
+
+  const mapped = farms.reduce((a: any, p: any) => {
+    const findToken = spicyTokens.find((token) => token.tag === `${p.key.fa2_address}:${p.key.token_id}`);
+    const findPool = spicyPools.find((pool) => pool.contract === p.key.fa2_address);
+    const findConfig = activeConfig.find((config: any) => config.key.fa2_address === p.key.fa2_address);
+    const findBalance = balances.find((balance: any) => balance.contract.address === p.key.fa2_address);
+
+    if (findConfig) {
+      a.push({
+        key: p.key,
+        value: p.value,
+        ...(findPool && {
+          pool: {
+            supply: p.supply,
+            ...findPool,
+            decimals: 18,
+            balance: findBalance.balance,
+          },
+          symbol: p.symbol,
+        }),
+        ...(findToken && {
+          token: {
+            supply: p.supply,
+            ...findToken,
+            balance: findBalance.balance,
+          },
+          symbol: p.symbol,
+        }),
+        single: !findPool,
+        rps: Number(findConfig.value.reward_per_sec),
+      });
+    }
+
+    return a;
+  }, []);
+
+  return mapped;
+};
+
+export const mapAccounts = (accounts: any, farms: any) => {
+  const mapped = accounts.reduce((map: any, current: any) => {
+    const address = current.key.user_address;
+    const grouped = map.get(address);
+
+    const farm = farms.find((f: any) => f.key.fa2_address === current.key.token.fa2_address);
+    const farmValue = farm && current.value.staked !== 0 ? Number(lpToTez(BigNumber(current.value.staked), farm)) : 0;
+    const farmReserve = farm && farm.pool ? farm.pool.reserve : 0;
+    const symbol = farm ? farm.symbol : '';
+
+    // eslint-disable-next-line no-param-reassign
+    current.totalValue = Number(farmValue);
+
+    if (farm && Number(farmValue) !== 0) {
+      const farmBalance = farm.pool ? farm.pool.balance : farm.token.balance;
+
+      if (!grouped) {
+        map.set(address, {
+          totalValue: current.totalValue,
+          farms: {
+            ...current.farms,
+            ...(farmValue !== 0 && {
+              [current.key.token.fa2_address]: {
+                tokenId: current.key.token.token_id,
+                reward: BigNumber(current.value.reward),
+                staked: Number(current.value.staked),
+                contract: current.key.token.fa2_address,
+                totalPercent: '0',
+                value: Number(farmValue),
+                symbol,
+                reserve: farmReserve,
+                balance: Number(farmBalance),
+              },
+            }),
+          },
+        });
+      } else {
+        map.set(address, {
+          ...grouped,
+          totalValue: Number(grouped.totalValue) + Number(current.totalValue),
+          farms: {
+            ...grouped.farms,
+            ...(farmValue !== 0 && {
+              [current.key.token.fa2_address]: {
+                tokenId: current.key.token.token_id,
+                reward: BigNumber(current.value.reward),
+                staked: Number(current.value.staked),
+                contract: current.key.token.fa2_address,
+                totalPercent: '0',
+                value: Number(farmValue),
+                symbol,
+                reserve: farmReserve,
+                balance: Number(farmBalance),
+              },
+            }),
+          },
+        });
+      }
+    }
+
+    return map;
+  }, new Map());
+
+  return mapped;
+};
 
 const mapFarms = async (farm: any) => {
   return {
