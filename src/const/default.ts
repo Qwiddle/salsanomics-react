e9fd797ba00fe66760631a4cb8f526da547d0983
Andrew <andjruben@gmail.com>
2022-10-27 20:46:01 -0400

    create event operation blacklist const

--- src/const/default.ts
+++ src/const/default.ts
@@ -1,2 +1,8 @@
 export const APP_NAME = 'Salsanomics';
 export const NETWORK = 'ghostnet';
+
+// operations calling startContest tied to events that we don't want to display
+export const opBlacklist: string[] = [
+  'opJJVpbLYJqeG1oDgGoC7ru2AzuwVM9HSaaQkEq6bhQedawZTx6',
+  'opT4mSFWEasHSkKi2hhVEnUr6LCS9KtyxJAnmzwQRUmYiLAfq3W',
+];
