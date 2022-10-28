18a0625f81d7c0b95463bf4b19eeba220ab2d578
Andrew <andjruben@gmail.com>
2022-10-27 21:26:42 -0400

    get active matter farms & map array

--- src/hooks/api/matter.ts
+++ src/hooks/api/matter.ts
@@ -1,5 +1,14 @@
-import { TZKT_API } from './tzkt';
+import { getSupply, getTokenName, TZKT_API } from './tzkt';
 import { MTTR } from '../../const/ecosystem';
+
+const mapFarms = async (farm: any) => {
+  return {
+    symbol: await getTokenName(farm.key.fa2_address),
+    key: farm.key,
+    value: farm.value,
+    supply: await getSupply(farm.key.fa2_address, farm.key.token_id),
+  };
+};
 
 export const getAccountsInternal = async () => {
   const req = `${TZKT_API}/contracts/${MTTR}/bigmaps/accounts_internal/keys?limit=1000`;
@@ -22,4 +31,16 @@ export const getMatterConfigs = async () => {
   const { configs } = json;
 
   return configs;
+};
+
+export const getMatterFarms = async () => {
+  const req = `${TZKT_API}/contracts/${MTTR}/bigmaps/farms_internal/keys`;
+  const res = await fetch(req);
+
+  if (!res.ok) throw new Error("Couldn't retrieve active matter farms");
+
+  const json = await res.json();
+  const farms = await Promise.all(json.map(mapFarms));
+
+  return farms;
 };
