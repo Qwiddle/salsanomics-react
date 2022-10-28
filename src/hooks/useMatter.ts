4598a7ace97036e0691d4742bbaf33b50e98b80d
Andrew <andjruben@gmail.com>
2022-10-27 22:52:06 -0400

    create useMatter hook

---
+++ src/hooks/useMatter.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import {
+  mapAccounts,
+  matchFarms,
+  getMatterBalances,
+  getMatterConfigs,
+  getMatterFarms,
+  getAccountsInternal,
+} from './api/matter';
+import { fetchSpicyPools, fetchSpicyTokens } from './api/spicy';
+
+const useMatter = () => {
+  const [accounts, setAccounts] = useState();
+
+  const fetchAll = async () => {
+    const mAccounts = await getAccountsInternal();
+    const mFarms = await getMatterFarms();
+    const mConfigs = await getMatterConfigs();
+    const mTokens = await fetchSpicyTokens();
+    const mPools = await fetchSpicyPools();
+    const mBalances = await getMatterBalances();
+
+    const matchedFarms = matchFarms(mPools, mTokens, mFarms, mConfigs, mBalances);
+    const matchedAccounts = mapAccounts(mAccounts, matchedFarms);
+
+    setAccounts(matchedAccounts);
+  };
+
+  useEffect(() => {
+    fetchAll();
+  }, []);
+
+  return {
+    accounts,
+  };
+};
+
+export default useMatter;
